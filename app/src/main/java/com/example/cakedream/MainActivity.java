415c02573d5974c97188e6d4525d98abc43e4690
kinjal-14 <kunjadiyakinjal1407@gmail.com>
2022-03-15 23:50:36 -0400

    updated splash screen

--- app/src/main/java/com/example/cakedream/MainActivity.java
+++ app/src/main/java/com/example/cakedream/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.cakedream;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME = 7000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,signup_screen.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME);
     }
 }
