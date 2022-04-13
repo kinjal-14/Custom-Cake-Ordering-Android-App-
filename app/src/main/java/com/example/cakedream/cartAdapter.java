48989588ae19efd6b887dde1fdcee233aa801c7c
PriyaGondaliya <priyagondaliya444@gmail.com>
2022-04-13 09:12:55 -0400

    updated cart screen backend

--- app/src/main/java/com/example/cakedream/cartAdapter.java
+++ app/src/main/java/com/example/cakedream/cartAdapter.java
@@ -1,50 +1,97 @@
 package com.example.cakedream;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class cartAdapter extends RecyclerView.Adapter<cartAdapter.MyViewHolder>{
-    private final ArrayList<cart_product_data> cartProductData;
+    private final ArrayList<cart_model> cart_product_data;
     FirebaseFirestore firestore;
-    String uId;
+    String uid;
 
-    public cartAdapter(ArrayList<cart_product_data> cartProductData) {
-        this.cartProductData = cartProductData;
+    public cartAdapter(ArrayList<cart_model> cart_product_data) {
+        this.cart_product_data = cart_product_data;
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
+
+        FirebaseUser mFirebaseUser = mAuth.getCurrentUser();
+        assert mFirebaseUser != null;
+        uid = mFirebaseUser.getUid();
 
 
     }
 
+
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        private final  ImageView cart_product_img;
+       private final TextView cake_name;
+       private final TextView qty;
+       private final TextView cart_card_price;
+       private final ImageButton cart_remove;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            cart_product_img = itemView.findViewById(R.id.cart_product_img);
+            cake_name = itemView.findViewById(R.id.cake_name);
+            qty = itemView.findViewById(R.id.qty);
+            cart_card_price = itemView.findViewById(R.id.cart_card_price);
+            cart_remove = itemView.findViewById(R.id.cart_remove);
         }
     }
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_card, parent, false);
+        return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        String name = cart_product_data.get(position).getName();
+        String qty = cart_product_data.get(position).getQuantity();
+        String price = String.valueOf(cart_product_data.get(position).getPrice());
 
+        holder.cake_name.setText(name);
+        holder.cart_card_price.setText(price);
+        holder.qty.setText(qty);
+        Picasso.get()
+                .load(cart_product_data.get(position).getImage())
+                .placeholder(R.drawable.home_img)
+                .error(R.drawable.home_img)
+                .into(holder.cart_product_img);
+
+        holder.cart_remove.setOnClickListener(view -> removeItemCart(cart_product_data.get(position).getProductid(), view.getContext()));
+
+    }
+
+    private void removeItemCart(String productid, Context context) {
+        firestore.collection("users").document(uid).collection("cart").document(productid)
+                .delete()
+                .addOnSuccessListener(unused -> {
+                    Toast.makeText(context, "Item removed From Favourite successfully!", Toast.LENGTH_SHORT).show();
+                    ((cart_screen) context).reloadActivity();
+                })
+                .addOnFailureListener(e -> Log.w("error", e.toString()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return cart_product_data.size();
     }
-
-
 }
--- app/src/main/java/com/example/cakedream/cart_screen.java
+++ app/src/main/java/com/example/cakedream/cart_screen.java
@@ -1,31 +1,155 @@
 package com.example.cakedream;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class cart_screen extends AppCompatActivity {
     ImageButton backbtn;
     AppCompatButton checkOutButton;
-    LinearLayout withdata, noData;
-    private final ArrayList<cart_product_data> cartProductData = new ArrayList<>();
+    ArrayList<AddiProductData> addiProductItemData = new ArrayList<AddiProductData>();;
+    ArrayList<cart_model> cart_product_data = new ArrayList<>();
+    LinearLayoutManager linearLayoutManager,linearLayoutManager1;
+    LinearLayout withdata, withoutdata;
+    RecyclerView recyclerView, recyclerView1;
+    cartAddiAdapter cartAddiAdapter;
+    cartAdapter cartAdapter;
     FirebaseFirestore firestore;
     String uid;
+    double subtotal = 0.0;
     private RecyclerView cartRecycleView;
     
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.cart_screen);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+        FirebaseUser mFirebaseUser = mAuth.getCurrentUser();
+        if (mFirebaseUser != null) {
+            uid = mFirebaseUser.getUid();
+        } else {
+            uid = null;
+        }
+
+        withdata = findViewById(R.id.with_data);
+        withoutdata = findViewById(R.id.no_item_found);
+
+        recyclerView = findViewById(R.id.addi_product_slider);
+        recyclerView1 = findViewById(R.id.cart_recyclerview);
+
+        checkOutButton = findViewById(R.id.checkout_btn);
+
+
+        setData();
+     checkOutButton.setOnClickListener(view -> {
+        Intent intent  = new Intent(getBaseContext(), shipping.class);
+        intent.putExtra("SubTotal", subtotal);
+              startActivity(intent);
+           });
+
+
+
+        setProductInformation();
+    }
+
+
+    private void setData() {
+        firestore.collection("users").document(uid).collection("cart").get()
+                                            .addOnSuccessListener(queryDocumentSnapshots -> {
+                                                if (!queryDocumentSnapshots.isEmpty()) {
+
+                                                    List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                                                    for (DocumentSnapshot d : list) {
+                                                        cart_model productData = d.toObject(cart_model.class);
+                                                        assert productData != null;
+                                                        String itemId = productData.getProductid();
+                                                        Log.d("itemid",itemId);
+                                                        double price = Double.valueOf(productData.getPrice());
+                                                        double count = Double.valueOf(productData.getQuantity());
+                                                        subtotal = subtotal + (price * count);
+                                                        Log.d("total", String.valueOf(subtotal));
+                                                        cart_product_data.add(productData);
+                                                        Log.d("data", String.valueOf(cart_product_data));
+                                                    }
+                                                } else {
+                                                    Toast.makeText(getBaseContext(), "No data found in Database", Toast.LENGTH_SHORT).show();
+
+                                                }
+                                            }).addOnFailureListener(e -> Toast.makeText(getBaseContext(), "Fail to get the data.", Toast.LENGTH_SHORT).show())
+                .addOnCompleteListener(task  -> setCartAdapter());
+
+                }
+
+
+    private void setCartAdapter() {
+        cartAdapter = new cartAdapter(cart_product_data);
+        recyclerView1.setAdapter(cartAdapter);
+        recyclerView1.setHasFixedSize(true);
+        linearLayoutManager1 = new LinearLayoutManager(cart_screen.this);
+        linearLayoutManager1.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView1.setLayoutManager(linearLayoutManager1);
+    }
+
+
+    private void setProductInformation() {
+        firestore.collection("AddProducts").get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    if(!queryDocumentSnapshots.isEmpty()){
+                        List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot d:list){
+                            AddiProductData data = d.toObject(AddiProductData.class);
+                            assert data != null;
+                            addiProductItemData.add(data);
+                        }
+                    }else {
+                        Toast.makeText(this, "products are not avalible for this time", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(e -> Toast.makeText(this, "Opps!,Fail to fetch data", Toast.LENGTH_SHORT).show())
+                .addOnCompleteListener(task -> setAdapter());
+    }
+
+    private void setAdapter() {
+        cartAddiAdapter = new cartAddiAdapter(addiProductItemData);
+        recyclerView.setAdapter(cartAddiAdapter);
+        recyclerView.setHasFixedSize(true);
+        linearLayoutManager = new LinearLayoutManager(cart_screen.this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    public void reloadActivity() {
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
     }
 }
+
