74e5dfe6d55f2f142af8614228e4c5d0b2acef1f
Shivanee22 <shivaneepatel58@gmail.com>
2022-04-20 11:11:42 -0400

    tablayout beckend for admin

---
+++ app/src/main/java/com/example/cakedream/Delivered_Fragment.java
@@ -0,0 +1,84 @@
+package com.example.cakedream;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Delivered_Fragment extends Fragment {
+    FirebaseFirestore firestore;
+    user_delivered_order_adapter adapter;
+    RecyclerView user_delivered_recyclerview;
+    ArrayList<orderdata> orderdata6;
+    String uid;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        firestore = FirebaseFirestore.getInstance();
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_delivery, container, false);
+        firestore = FirebaseFirestore.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser mFirebaseUser = mAuth.getCurrentUser();
+        assert mFirebaseUser != null;
+        uid = mFirebaseUser.getUid();
+        user_delivered_recyclerview= view.findViewById(R.id.user_delivered_recyclerview);
+        user_delivered_recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
+        orderdata6 = new ArrayList<>();
+        adapter = new user_delivered_order_adapter(orderdata6);
+        user_delivered_recyclerview.setAdapter(adapter);
+        setdata();
+
+        return view;
+    }
+
+    private void setdata() {
+        firestore.collection("users").document(uid).collection("order").whereEqualTo("ordertype","completed").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                List<DocumentSnapshot> snapshotList1 = queryDocumentSnapshots.getDocuments();
+                for (DocumentSnapshot snapshot1 : snapshotList1) {
+                    Log.d("data", snapshot1.getData().toString());
+                    orderdata data = snapshot1.toObject(orderdata.class);
+                    assert data != null;
+                    orderdata6.add(data);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("no data", String.valueOf(e));
+            }
+        });
+    }
+}
